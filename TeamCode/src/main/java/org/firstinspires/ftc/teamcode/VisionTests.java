14e9eb0bf9e75450fc593034243e61db15bffef1
bkleinh <74694427+bkleinh@users.noreply.github.com>
2023-12-11 19:09:05 -0500

    fix a crash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTests.java
@@ -16,9 +16,9 @@ public class VisionTests extends LinearOpMode {
     HydraObjDetDatalogger mObjDetDatalogger;
     @Override
     public void runOpMode() throws InterruptedException {
+        mOpmode = new HydraOpMode(telemetry, hardwareMap, null, mObjDetDatalogger);
         mObjDet = new HydraObjectDetect(mOpmode, "Blue_Prop.tflite");
         mObjDetDatalogger = new HydraObjDetDatalogger("obj-log-");
-        mOpmode = new HydraOpMode(telemetry, hardwareMap, null, mObjDetDatalogger);
         List<LynxModule> hubs = hardwareMap.getAll(LynxModule.class);
         for (LynxModule mod : hubs) {
             mod.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
